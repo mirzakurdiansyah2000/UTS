91cb18a4c4ed40c31897fd1fdf6ba3e35f8f1618
mirzakurdiansyah2000 <47847173+mirzakurdiansyah2000@users.noreply.github.com>
2019-05-02 16:50:29 +0700

    Add files via upload

---
+++ content-left.php
@@ -0,0 +1,114 @@
+<aside class="menu-sidebar2 js-right-sidebar d-block d-lg-none">
+                <div class="logo">
+                    <a href="#">
+                        <img src="cool-admin/images/icon/logo-white.png" alt="Cool Admin" />
+                    </a>
+                </div>
+                <div class="menu-sidebar2__content js-scrollbar2">
+                    <div class="account2">
+                        <div class="image img-cir img-120">
+                            <img src="cool-admin/images/icon/avatar-big-01.jpg" alt="John Doe" />
+                        </div>
+                        <h4 class="name">john doe</h4>
+                        <a href="#">Sign out</a>
+                    </div>
+                    <nav class="navbar-sidebar2">
+                        <ul class="list-unstyled navbar__list">
+                            <li class="active has-sub">
+                                <a class="js-arrow" href="#">
+                                    <i class="fas fa-tachometer-alt"></i>Latihan
+                                    <span class="arrow">
+                                        <i class="fas fa-angle-down"></i>
+                                    </span>
+                                </a>
+                                <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                    <li>
+                                        <a href="index.php?page=soal-uts">
+                                            <i class="fas fa-tachometer-alt"></i>Soal UTS</a>
+                                    </li>
+                                    <li>
+                                       
+                                   
+                                </ul>
+                            </li>
+                           
+                            <li class="has-sub">
+                                <a class="js-arrow" href="#">
+                                    <i class="fas fa-copy"></i>Pages
+                                    <span class="arrow">
+                                        <i class="fas fa-angle-down"></i>
+                                    </span>
+                                </a>
+                                <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                    <li>
+                                        <a href="login.html">
+                                            <i class="fas fa-sign-in-alt"></i>Login</a>
+                                    </li>
+                                    <li>
+                                        <a href="register.html">
+                                            <i class="fas fa-user"></i>Register</a>
+                                    </li>
+                                    <li>
+                                        <a href="forget-pass.html">
+                                            <i class="fas fa-unlock-alt"></i>Forget Password</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="has-sub">
+                                <a class="js-arrow" href="#">
+                                    <i class="fas fa-desktop"></i>UI Elements
+                                    <span class="arrow">
+                                        <i class="fas fa-angle-down"></i>
+                                    </span>
+                                </a>
+                                <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                    <li>
+                                        <a href="button.html">
+                                            <i class="fab fa-flickr"></i>Button</a>
+                                    </li>
+                                    <li>
+                                        <a href="badge.html">
+                                            <i class="fas fa-comment-alt"></i>Badges</a>
+                                    </li>
+                                    <li>
+                                        <a href="tab.html">
+                                            <i class="far fa-window-maximize"></i>Tabs</a>
+                                    </li>
+                                    <li>
+                                        <a href="card.html">
+                                            <i class="far fa-id-card"></i>Cards</a>
+                                    </li>
+                                    <li>
+                                        <a href="alert.html">
+                                            <i class="far fa-bell"></i>Alerts</a>
+                                    </li>
+                                    <li>
+                                        <a href="progress-bar.html">
+                                            <i class="fas fa-tasks"></i>Progress Bars</a>
+                                    </li>
+                                    <li>
+                                        <a href="modal.html">
+                                            <i class="far fa-window-restore"></i>Modals</a>
+                                    </li>
+                                    <li>
+                                        <a href="switch.html">
+                                            <i class="fas fa-toggle-on"></i>Switchs</a>
+                                    </li>
+                                    <li>
+                                        <a href="grid.html">
+                                            <i class="fas fa-th-large"></i>Grids</a>
+                                    </li>
+                                    <li>
+                                        <a href="fontawesome.html">
+                                            <i class="fab fa-font-awesome"></i>FontAwesome</a>
+                                    </li>
+                                    <li>
+                                        <a href="typo.html">
+                                            <i class="fas fa-font"></i>Typography</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </aside>
